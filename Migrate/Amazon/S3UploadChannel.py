f6119fded503a499268fc72204ff8584c4f7a6b0
feoff <feoff3@gmail.com>
2013-03-21 22:14:34 +0400

    added xml uploading

--- Migrate/Amazon/S3UploadChannel.py
+++ Migrate/Amazon/S3UploadChannel.py
@@ -362,9 +362,7 @@ class S3UploadChannel(object):
         
         manifest.finalize()
 
-        #TODO: try again with close
-        # now we ignore the xml so ec2import will create it itself but will get the our data
-        
+               
         s3key = Key(self.__bucket , xmlkey)
         s3key.set_contents_from_filename(xmltempfile);
         s3key.close()
