cdadc4a4bf45a847fcb40d95f8c0ef9073de4e52
feoff <feoff3@gmail.com>
2013-11-24 21:25:00 +0400

    deleted extra files

--- Migrate/CloudSigma/cloudsigma/conf.py
+++
@@ -1,9 +0,0 @@
-"""Configuration of the client."""
-import os
-
-CONFIG_LOCATION = os.getenv('CLOUDSIGMA_CONFIG',
-                            os.path.join(os.path.expanduser('~'), '.cloudsigma.conf')
-                            )
-
-from configobj import ConfigObj
-config = ConfigObj(CONFIG_LOCATION)
