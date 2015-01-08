c853de2de51204fd6464c34fd80daa2787eae098
feoff <feoff3@gmail.com>
2015-01-08 21:35:35 +0300

    see #575, added more dbg output for possible data loss

--- Migrate/SimpleDataTransferProto.py
+++ Migrate/SimpleDataTransferProto.py
@@ -6,6 +6,7 @@ __copyright__ = "Copyright (C) 2013 Migrate2Iaas"
 import DataTransferProto
 import ImageMedia
 import DataExtent
+import logging
 
 class SimpleDataTransferProto(DataTransferProto.DataTransferProto):
     """Data transfer protocol to just pass the data to the underlying media"""
@@ -15,7 +16,12 @@ class SimpleDataTransferProto(DataTransferProto.DataTransferProto):
 
     def writeData(self , extents):
         for extent in extents:
-            self.__media.writeDiskData(extent.getStart() , extent.getData())
+            start = extent.getStart()
+            data = extent.getData()
+            size = extent.getSize()
+            if not (size == len(data)):
+                logging.warning(" Data chunk written to disk at offset " + str(start) + " data size mismatch!")
+            self.__media.writeDiskData(start , data)
 
     def readData(self , extent):
         return self.__media.readDiskData(extent.getStart() , extent.getSize())
