52e9835c56268665707353362243ac15fc27b82e
feoff <feoff3@gmail.com>
2015-01-19 06:16:54 +0300

    added ip reconfigure

--- resources/CloudscraperBootAdjust/adjust/adjust.cmd
+++ resources/CloudscraperBootAdjust/adjust/adjust.cmd
@@ -1,3 +1,5 @@
+SETLOCAL EnableDelayedExpansion
+
 :: installs virtio networks\drivers etc
 
 echo "Preved!" >> C:\adjustlog.txt
@@ -15,3 +17,26 @@ if defined ProgramFiles(x86) (
 certutil -addstore TrustedPublisher redhat.cer >> C:\adjustlog.txt
 
 %~dp0\%DEVCON% install "%DRVPATH%\netkvm.inf" "PCI\VEN_1AF4&DEV_1000&SUBSYS_00011AF4&REV_00" >> C:\adjustlog.txt
+
+if exist %~dp0\netsh_dump.txt (
+
+:: Checks adapter name
+SET adapterName=
+
+FOR /F "tokens=* delims=:" %%a IN ('IPCONFIG ^| FIND /I "ETHERNET ADAPTER"') DO (
+SET adapterName=%%a
+
+REM Removes "Ethernet adapter" from the front of the adapter name
+SET adapterName=!adapterName:~17!
+
+REM Removes the colon from the end of the adapter name
+SET adapterName=!adapterName:~0,-1!
+
+:: renaming the primary ethernet to Local Area Network
+netsh interface set interface name="!adapterName!" newname=”Local Area Connection” >> C:\adjustlog.txt
+)
+:: executing the adjust script
+echo "Importing tcpip settings" >> C:\adjustlog.txt
+netsh -f %~dp0\netsh_dump.txt >> C:\adjustlog.txt
+)
+:EOF
