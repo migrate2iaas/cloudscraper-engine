584735f366ee787481124eefdd9d1dd1d9f6cc0b
feoff <feoff3@gmail.com>
2015-02-18 21:11:10 +0300

    fixed the inf path for pnputil

--- resources/CloudscraperBootAdjust/preprocess.cmd
+++ resources/CloudscraperBootAdjust/preprocess.cmd
@@ -2,4 +2,4 @@ rem if we run from 32-bit cmd on 64-bit windows
 if exist C:\Windows\sysnative\pnputil.exe cd C:\Windows\sysnative
 
 certutil -addstore TrustedPublisher "%~dp0"\adjust\redhat.cer
-pnputil /a "%~dp0"\virtio\viostor.inf
+pnputil /a "%~dp0\virtio\viostor.inf"
