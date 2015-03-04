2cd29dda6cac1cd0109ce3bd36ca387a59c381bb
feoff <feoff3@gmail.com>
2015-03-04 22:24:39 +0300

    see #672, updated network detection script

--- resources/CloudscraperBootAdjust/adjust/adjust.cmd
+++ resources/CloudscraperBootAdjust/adjust/adjust.cmd
@@ -20,7 +20,8 @@ certutil -addstore TrustedPublisher redhat.cer >> C:\adjustlog.txt
 "%~dp0\%DEVCON%" install "%DRVPATH%\netkvm.inf" "PCI\VEN_1AF4&DEV_1000&SUBSYS_00011AF4&REV_00" >> C:\adjustlog.txt
 
 echo Wait till network device is up... >>  C:\adjustlog.txt
-ping 128.0.0.1 -n 1 -w 600 >>  C:\adjustlog.txt
+ping 128.0.0.1 -n 5 -w 600 >>  C:\adjustlog.txt
+IPCONFIG >> C:\adjustlog.txt
 
 if exist %~dp0\netsh_dump.txt (
 
@@ -40,11 +41,14 @@ SET adapterName=!adapterName:~17!
 REM Removes the colon from the end of the adapter name
 SET adapterName=!adapterName:~0,-1!
 
+echo Renaming !adapterName! to Local Area Connection >> C:\adjustlog.txt
 :: renaming the primary ethernet to Local Area Network
 netsh interface set interface name="!adapterName!" newname="Local Area Connection" >> C:\adjustlog.txt
 )
 :: executing the adjust script
 echo "Importing tcpip settings" >> C:\adjustlog.txt
 netsh -f "%~dp0\netsh_dump.txt" >> C:\adjustlog.txt
+ping 128.0.0.1 -n 1 -w 6000 >>  C:\adjustlog.txt
+del /Q "%~dp0\netsh_dump.txt" 
 )
 :EOF
