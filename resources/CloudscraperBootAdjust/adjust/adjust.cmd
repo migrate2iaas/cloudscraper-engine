deb0776964007cce3f595766eca025795b20bedd
feoff <feoff3@gmail.com>
2015-03-15 19:51:09 +0300

    see #672, rebuilt search code

--- resources/CloudscraperBootAdjust/adjust/adjust.cmd
+++ resources/CloudscraperBootAdjust/adjust/adjust.cmd
@@ -20,7 +20,7 @@ certutil -addstore TrustedPublisher redhat.cer >> C:\adjustlog.txt
 "%~dp0\%DEVCON%" install "%DRVPATH%\netkvm.inf" "PCI\VEN_1AF4&DEV_1000&SUBSYS_00011AF4&REV_00" >> C:\adjustlog.txt
 
 echo Wait till network device is up... >>  C:\adjustlog.txt
-ping 128.0.0.1 -n 5 -w 600 >>  C:\adjustlog.txt
+ping 128.0.0.1 -n 1000 -w 1 >>  C:\adjustlog.txt
 IPCONFIG >> C:\adjustlog.txt
 
 if exist %~dp0\netsh_dump.txt (
@@ -49,11 +49,15 @@ netsh interface set interface name="!adapterName!" newname="Local Area Connectio
 echo "Importing tcpip settings" >> C:\adjustlog.txt
 netsh -f "%~dp0\netsh_dump.txt" >> C:\adjustlog.txt
 ping 128.0.0.1 -n 1 -w 6000 >>  C:\adjustlog.txt
-del /Q "%~dp0\netsh_dump.txt" >>  C:\adjustlog.txt
+IF ERRORLEVEL 0 (
+	echo Netconf applied. Deleting the network config file >>  C:\adjustlog.txt
+	del /Q "%~dp0\netsh_dump.txt" >>  C:\adjustlog.txt
+)
 
 )
 
 echo "Disable autoadjust" >>   C:\adjustlog.txt
+sc query CloudscraperBoot  >>   C:\adjustlog.txt
 sc config CloudscraperBoot start=demand >>   C:\adjustlog.txt
 
 :EOF
