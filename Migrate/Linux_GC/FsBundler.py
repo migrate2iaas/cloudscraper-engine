52704c4059ff022fdc979248b8d2313266492f32
feoff <feoff3@gmail.com>
2015-06-04 20:46:40 +0300

    see #686, typo fixed

--- Migrate/Linux_GC/FsBundler.py
+++ Migrate/Linux_GC/FsBundler.py
@@ -32,7 +32,7 @@ class FsBundler(block_disk.RootFsRaw):
 
     def __init__(self, fs_size, fs_type, skip_disk_space_check, diskname = "disk.raw" , statvfs = os.statvfs):
         #override to use ndb
-        NbdBundle_utils.NdbOverride.init_override()
+        NbdBundle_utils.NbdOverride.init_override()
 
         super(FsBundler , self).__init__(fs_size, fs_type, skip_disk_space_check, statvfs , diskname)
 
