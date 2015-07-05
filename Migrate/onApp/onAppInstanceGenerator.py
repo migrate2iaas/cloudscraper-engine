e50ae1ebc7c9169b525000d82b8f53695f04e801
feoff <feoff3@gmail.com>
2015-07-05 23:13:43 +0300

    set ssl enabled

--- Migrate/onApp/onAppInstanceGenerator.py
+++ Migrate/onApp/onAppInstanceGenerator.py
@@ -26,7 +26,10 @@ class OnAppBase:
         basicAuth = "";
         def connectOnApp(self, username, password, hostname, port):
                 self.basicAuth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '');
-                self.conn = httplib.HTTPConnection(hostname, port);
+                if int(port)==443:
+                    self.conn = httplib.HTTPSConnection(hostname, port);
+                else:
+                    self.conn = httplib.HTTPConnection(hostname, port);
                 try:
                         logging.info("Connecting to HTTPConnection " + hostname);
                         self.conn.connect();
