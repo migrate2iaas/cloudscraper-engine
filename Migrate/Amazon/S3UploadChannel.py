e9db14cd2bcb509a1e6a132ca2195bff5806a502
alxnapalm <alx-k@mail.ru>
2016-01-11 22:56:57 +0300

    see #812, make upload queue smaller

--- Migrate/Amazon/S3UploadChannel.py
+++ Migrate/Amazon/S3UploadChannel.py
@@ -295,7 +295,7 @@ class S3UploadChannel(UploadChannel.UploadChannel):
     #chunk size means one data element to be uploaded. it waits till all the chunk is transfered to the channel than makes an upload (not fully implemented)
     def __init__(
             self, bucket, awskey, awssercret, resultDiskSizeBytes, location='', keynameBase=None, diskType='VHD',
-            resume_upload=False, chunksize=10*1024*1024, upload_threads=4, queue_size=16, use_ssl=True,
+            resume_upload=False, chunksize=10*1024*1024, upload_threads=4, queue_size=6, use_ssl=True,
             manifest_path=None, increment_depth=1, walrus=False, walrus_path="/services/WalrusBackend",
             walrus_port=8773, make_link_public=False):
         self.__uploadQueue = Queue.Queue(queue_size)
