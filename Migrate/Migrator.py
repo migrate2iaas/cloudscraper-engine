c81bfb9e14fda261a41ba0519673ccafba14e71f
feoff <feoff3@gmail.com>
2016-02-10 19:58:47 +0300

    updated missed volume attach

--- Migrate/Migrator.py
+++ Migrate/Migrator.py
@@ -619,8 +619,8 @@ class Migrator(object):
             return True
 
         if vol:
-            #TODO: add volume attach here
-            logging.info(">>>>>>>>>>>>>>>>> The data volume " + str(volname) + " created. You could add it to your server via " + self.__cloudName + " management console")
+            vol.attach(self.__resultingInstance.getId())
+            logging.info(">>>>>>>>>>>>>>>>> The data volume " + str(volname) + " created and added to VM instance " + self.__resultingInstance.getId())
             return True
 
         return False
